85d7bf1631b25afbe892fc529fd7c219c72d5e54
ydvdhrj <ydv.dhrj123@gmail.com>
2025-02-23 04:03:21 +0530

    third commit

--- app/src/main/java/com/example/notessqlite/MainActivity.kt
+++ app/src/main/java/com/example/notessqlite/MainActivity.kt
@@ -18,13 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
 
         db = NotesDatabaseHelper(this)
